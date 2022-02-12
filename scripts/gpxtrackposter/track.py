3a47f527d30081871b7a3d1ee7cd73b6b23db589
superleeyom <635709492@qq.com>
2022-02-12 17:02:42 +0800

    fix bug

--- scripts/gpxtrackposter/track.py
+++ scripts/gpxtrackposter/track.py
@@ -205,7 +205,7 @@ class Track:
             "elapsed_time": datetime.timedelta(
                 seconds=(moving_data.moving_time + moving_data.stopped_time)
             ),
-            "average_speed": moving_data.moving_distance / moving_data.moving_time if moving_data.moving_time else None,
+            "average_speed": moving_data.moving_distance / moving_data.moving_time if moving_data.moving_time else 0,
         }
 
     def to_namedtuple(self):
